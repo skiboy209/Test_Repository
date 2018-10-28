247d0b42cf09ae9ce674b260eec7c49f67deb01a
skiboy209 <dallanrocks@gmail.com>
2018-10-28 14:14:51 -0600

    Updted css html and js

--- script.js
+++ script.js
@@ -8,12 +8,6 @@ $(document).ready( function() {
   var services = "<p class='component'>We provide excavation solutions for all your building needs.</p><p class='component'> From the driveway to the foundation </p>"
   var employment = "<p class='component'>If you are inquiring on employment through Burns Excavation</p><p class='component'> call Jeffrey Burns at 555-555-5555</p>"
   var contact = "<p class='component'>Contact Burns Excavation by email at info@burnsexcavation.com</p><p class='component'> at the following address Example St Spokane WA 66666.</p><ul class='component' style='list-style-type:none'><li>Phone: 555-555-5555</li><li>Cell Phone:555-555-5555</li><li>Main office Address: 1668 E 1440 N, Spokane WA 29024</li></ul>"
-  var homeImg = "<img class='componentImg' src='assets/drivewayOptimized.jpg'/>"
-  var aboutImg = "<img class='componentImg' src='assets/foundationcut4Optimized.jpg'/>"
-  var servicesImg = "<img class='componentImg' src='assets/driveway2Optimized.jpg'/>"
-  var employmentImg = "<img class='componentImg' src='assets/foundationcut3Optimized.jpg'/>"
-  var contactImg = "<img class='componentImg' src='assets/driveway3Optimized.jpg'/>"
-  var galleryTxt = "<p class='component'>Click the image to cycle through the gallery!</p>"
   
   var images = new Array()
   function preload() {
@@ -33,14 +27,6 @@ $(document).ready( function() {
     "assets/foundationcutOptimized.jpg"
   )
 
-  var gallery = [
-    homeImg,
-    servicesImg,
-    employmentImg,
-    contactImg,
-  ]
-
-  var test = 'componentImg';
   var idx = 0;
   
   //Content Displayer
@@ -77,7 +63,6 @@ $(document).ready( function() {
             $( "#componentDisplay" ).html(images[idx])
           }else{
             idx++;
-            console.log(idx);
             $( "#componentDisplay" ).html(images[idx])
           }
         })
