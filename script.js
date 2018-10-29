56bcbbe9cd7f5a4c6040ea2da2c7d3527256f9af
skiboy209 <dallanrocks@gmail.com>
2018-10-29 17:38:59 -0600

    Fixed wording

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 $(document).ready( function() {
   var home = "<p class='component'>We provide ground breaking solutions crafted with quality and integrity</p>"
-  var about = "<p class='component'>Burns Excavation was founded to bring you quality excavation solutions will be fulfilled with integrity.</p>"
+  var about = "<p class='component'>Burns Excavation was founded to bring you quality excavation solutions fulfilled with integrity.</p>"
   var services = "<p class='component'>We provide solutions for all your excavation needs.</p><ul id='servicesList'><li class='serviceListItem'>Site Development</li><li class='serviceListItem'>Clearing / Grading</li><li class='serviceListItem'>Drainage & More</li></ul>"
   var employment = "<p class='component'>Burns Excavation Employment</p><p class='component'> If you are seeking employment contact Jeff Burns, Owner at 360-878-3200</p>"
   var contact = "<p class='component'>Contact Burns Excavation <ul id='contactList'><li class='contactListItem'>Email: jeffburns@burnsexcavation.com</li><li class='contactListItem'>Phone: 360-878-3200</li><li class='contactListItem'>Address: PO box coming soon!</li></ul></p>"
