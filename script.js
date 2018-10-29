b3d4f89339909d51a674ae1e03ebf7c65153a084
skiboy209 <dallanrocks@gmail.com>
2018-10-29 13:53:38 -0600

    New logo and updated styling as well as scripts. Bug fixes.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //Runs script to initialize jquery functionality.
 
 $(document).ready( function() {
-  var home = "<p class='component'>Ground breaking solutions built with quality and integrity</p>"
+  var home = "<p class='component'>We provide ground breaking solutions crafted with quality and integrity</p>"
   var about = "<p class='component'>Burns Excavation was founded on the principle of creating </p><p class='component'> quality excavation solutions fulfilled with integrity.</p>"
   var services = "<p class='component'>We provide excavation solutions for all your building needs.</p><p class='component'> From the driveway to the foundation </p>"
   var employment = "<p class='component'>If you are inquiring on employment through Burns Excavation</p><p class='component'> call Jeffrey Burns at 555-555-5555</p>"
@@ -37,33 +37,39 @@ $(document).ready( function() {
   
   //Content Displayer
   $("#componentDisplay2").html(home);
-    $("#componentDisplay").html(images[0]);        
+    $("#componentDisplay").html(images[7]);        
     $( "#home" ).click(function() {
-      $("#componentDisplay").html(images[0]);        
+      $( "#componentDisplay" ).off('click');
+      $("#componentDisplay").html(images[7]);        
       $("#componentDisplay2").html(home);        
       });
-      $( "#about" ).click(function() {
-        $("#componentDisplay").html(images[1]);        
+      $( "#about" ).click(function() {    
+        $( "#componentDisplay" ).off('click');
+        $("#componentDisplay").html(images[1])
         $("#componentDisplay2").html(about);        
       });
       $( "#services" ).click(function() {
-        $("#componentDisplay").html(images[2]);        
+        $( "#componentDisplay" ).off('click');
+        $("#componentDisplay").html(images[2])    
         $("#componentDisplay2").html(services);        
       });
       $( "#employment" ).click(function() {
-        $("#componentDisplay").html(images[3]);        
+        $( "#componentDisplay" ).off('click');
+        $("#componentDisplay").html(images[3])       
         $("#componentDisplay2").html(employment);        
       });
       $( "#contact" ).click(function() {
-        $("#componentDisplay").html(images[4]);  
+        $( "#componentDisplay" ).off('click');
+        $("#componentDisplay").html(images[4]) 
         $("#componentDisplay2").html(contact);        
       });
       $( "#gallery" ).click(function() {
-        $( "#componentDisplay" ).html(images[0]);
+        $( "#componentDisplay" ).html(images[0])
         $( "#componentDisplay2" ).html(galleryTxt)
-      })
-      $( "#gallery" ).ready(function(){
-        $( "#componentDisplay" ).click(function(){
+
+        //Gallery logic
+        console.log("Gallery logic bound to image")
+        $( "#componentDisplay" ).on('click', function(){
           if(idx >= images.length - 1){
             idx = 0;
             $( "#componentDisplay" ).html(images[idx])
@@ -74,5 +80,8 @@ $(document).ready( function() {
           }
         })
       })
+ /*     $( "#gallery" ).ready(function(){
+
+      })*/
 });
 
